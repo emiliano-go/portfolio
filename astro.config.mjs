2c09130016e5885d3b25c14a654d470c14460751
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-06-26 22:53:12 -0300

    Add schemap code example card on projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,18 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    build: {
+      cssMinify: 'lightningcss',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules/motion')) {
+              return 'motion';
+            }
+          },
+        },
+      },
+    },
+  },
 });
